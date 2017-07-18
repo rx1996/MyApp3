e9865d4cf6c65aa507a39223339b2607f384243c
rx1996 <3343108599@qq.com>
2017-07-18 16:25:51 +0800

    第四次

--- app2/src/main/java/myapplication/liangcang/shop/activity/ShopInformationActivity.java
+++ app2/src/main/java/myapplication/liangcang/shop/activity/ShopInformationActivity.java
@@ -1,6 +1,8 @@
 package myapplication.liangcang.shop.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,11 +10,13 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.youth.banner.Banner;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
@@ -23,15 +27,25 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import myapplication.liangcang.R;
+import myapplication.liangcang.activity.MainActivity;
 import myapplication.liangcang.base.BaseActivity;
+import myapplication.liangcang.base.BaseFragment;
+import myapplication.liangcang.daren.fragment.DarenFragment;
+import myapplication.liangcang.fenxiang.fragment.FenxiangFragment;
+import myapplication.liangcang.geren.fragment.GerenFragment;
 import myapplication.liangcang.shop.bean.GiftParentInfo;
 import myapplication.liangcang.shop.bean.ShopInformationBean;
+import myapplication.liangcang.shop.fragment.ShopChanpinFragment;
+import myapplication.liangcang.shop.fragment.ShopFragment;
+import myapplication.liangcang.shop.fragment.ShopGushiFragment;
+import myapplication.liangcang.utils.GlideImageLoader;
+import myapplication.liangcang.zazhi.fragment.ZaZhiFragment;
 import okhttp3.Call;
 
 public class ShopInformationActivity extends BaseActivity {
 
     @Bind(R.id.banner)
-    ImageView banner;
+    Banner banner;
     @Bind(R.id.tv_name)
     TextView tvName;
     @Bind(R.id.tv_desc)
@@ -63,9 +77,12 @@ public class ShopInformationActivity extends BaseActivity {
     @Bind(R.id.rg_main)
     RadioGroup rgMain;
     @Bind(R.id.activity_shop_information)
-    RelativeLayout activityShopInformation;
+    ScrollView activityShopInformation;
     private String url;
     private int id;
+    private Fragment tempFragment;
+    private int position = 0;
+    private ArrayList<BaseFragment> fragments;
     @Override
     public void initListener() {
 
@@ -79,6 +96,66 @@ public class ShopInformationActivity extends BaseActivity {
                 .url(url)
                 .build()
                 .execute(new MyStringCallback());
+        initFragment();
+
+        //设置RadioGroup的选中监听
+        rgMain.setOnCheckedChangeListener(new MyOnCheckedChangeListener());
+
+        //设置默认选择首页
+        rgMain.check(R.id.rb_gushi);
+    }
+    class MyOnCheckedChangeListener implements RadioGroup.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId){
+                case R.id.rb_gushi:
+                    position = 0;
+                    break;
+                case R.id.rb_chanpin:
+                    position = 1;
+                    break;
+
+            }
+            Fragment currentFragment = fragments.get(position);
+            switchFragment(currentFragment);
+        }
+    }
+    private void switchFragment(Fragment currentFragment) {
+        if(currentFragment != tempFragment){//不是同一个
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+            if(!currentFragment.isAdded()){
+
+                //把之前的隐藏
+                if(tempFragment!= null){
+                    ft.hide(tempFragment);
+                }
+                //把现在的添加
+                ft.add(R.id.frameLayout,currentFragment);
+
+            }else{
+                //把之前的隐藏
+                if(tempFragment!= null){
+                    ft.hide(tempFragment);
+                }
+                //把当前的显示
+                ft.show(currentFragment);
+            }
+
+
+            //提交
+            ft.commit();
+
+            tempFragment = currentFragment;
+
+        }
+
+    }
+    private void initFragment() {
+        fragments = new ArrayList<>();
+        fragments.add(new ShopGushiFragment());
+        fragments.add(new ShopChanpinFragment());
     }
     class MyStringCallback extends StringCallback {
         @Override
@@ -110,21 +187,16 @@ public class ShopInformationActivity extends BaseActivity {
 //                .error(R.drawable.ic_login_logo)
 //                .diskCacheStrategy(DiskCacheStrategy.ALL)
 //                .into(banner);
+        List<String> images = new ArrayList<>();
+        for(int i = 0; i < bean.getData().getItems().getImages_item().size(); i++) {
+            images.add(bean.getData().getItems().getImages_item().get(i));
+        }
+        banner.setImages(images).setImageLoader(new GlideImageLoader()).start();
         tvName2.setText(bean.getData().getItems().getOwner_name());
     }
 
     @Override
     public int getLayoutId() {
         return R.layout.activity_shop_information;
-    }
-
-    @OnClick({R.id.rb_gushi, R.id.rb_chanpin})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.rb_gushi:
-                break;
-            case R.id.rb_chanpin:
-                break;
-        }
     }
 }
---
+++ app2/src/main/java/myapplication/liangcang/shop/fragment/ShopGushiFragment.java
@@ -0,0 +1,35 @@
+package myapplication.liangcang.shop.fragment;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import myapplication.liangcang.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/7/18.
+ */
+
+public class ShopGushiFragment extends BaseFragment {
+    private TextView textView;
+    @Override
+    public View initView() {
+        textView = new TextView(mContext);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(25);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("故事内容");
+    }
+
+    @Override
+    public void initTitle() {
+
+    }
+}
