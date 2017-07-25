ba74c1a60c5dd299821d943df9aa3db7c9b1da3c
rx1996 <3343108599@qq.com>
2017-07-25 20:01:22 +0800

    第二十二次

--- app2/src/main/java/myapplication/liangcang/shopcat/adapter/ShoppingCartAdapter.java
+++ app2/src/main/java/myapplication/liangcang/shopcat/adapter/ShoppingCartAdapter.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -17,6 +19,7 @@ import butterknife.ButterKnife;
 import myapplication.liangcang.R;
 import myapplication.liangcang.shop.bean.ShopInformationBean;
 import myapplication.liangcang.shopcat.activity.ShopCatActivity;
+import myapplication.liangcang.shopcat.view.AddSubView;
 
 /**
  * Created by Administrator on 2017/7/25.
@@ -25,6 +28,7 @@ import myapplication.liangcang.shopcat.activity.ShopCatActivity;
 public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapter.MyViewHolder> {
     private final ShopCatActivity context;
     private final ArrayList<ShopInformationBean> datas;
+
 
 
     public ShoppingCartAdapter(ShopCatActivity context, ArrayList<ShopInformationBean> datas) {
@@ -48,6 +52,10 @@ public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapte
                 diskCacheStrategy(DiskCacheStrategy.ALL).into(holder.ivGov);
         holder.tvDescGov.setText(infoBean.getData().getItems().getGoods_name());
         holder.tvPriceGov.setText("￥" + infoBean.getData().getItems().getPrice());
+        holder.nasGoodinfoNum.setValue(infoBean.getData().getItems().getNumber());
+        holder.nasGoodinfoNum.setMinValue(1);
+        //库存
+        holder.nasGoodinfoNum.setMaxValue(200);
 
     }
 
@@ -65,9 +73,36 @@ public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapte
         TextView tvDescGov;
         @Bind(R.id.tv_price_gov)
         TextView tvPriceGov;
+        @Bind(R.id.ll_1)
+        LinearLayout ll1;
+        @Bind(R.id.checkbox_all2)
+        CheckBox checkboxAll2;
+        @Bind(R.id.iv_gov2)
+        ImageView ivGov2;
+        @Bind(R.id.nas_goodinfo_num)
+        AddSubView nasGoodinfoNum;
+        @Bind(R.id.tv_desc_gov2)
+        TextView tvDescGov2;
+        @Bind(R.id.tv_price_gov2)
+        TextView tvPriceGov2;
+        @Bind(R.id.ll_2)
+        LinearLayout ll2;
+        @Bind(R.id.tv_shanchu)
+        TextView tvShanchu;
+        @Bind(R.id.rl_2)
+        RelativeLayout rl2;
+
         public MyViewHolder(View itemView) {
             super(itemView);
-            ButterKnife.bind(this,itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ShopInformationBean infoBean = datas.get(getLayoutPosition());
+                    infoBean.getData().getItems().setCheck(!infoBean.getData().getItems().isCheck());
+                    notifyItemChanged(getLayoutPosition());
+                }
+            });
         }
     }
 }
