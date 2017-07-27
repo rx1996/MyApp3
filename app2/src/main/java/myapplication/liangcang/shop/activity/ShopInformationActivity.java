74d00857ea24597cba610d215f0c806a2705ce0e
rx1996 <3343108599@qq.com>
2017-07-27 15:29:58 +0800

    第二十八次  分享

--- app2/src/main/java/myapplication/liangcang/shop/activity/ShopInformationActivity.java
+++ app2/src/main/java/myapplication/liangcang/shop/activity/ShopInformationActivity.java
@@ -36,6 +36,7 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import myapplication.liangcang.R;
 import myapplication.liangcang.base.BaseActivity;
 import myapplication.liangcang.base.BaseFragment;
@@ -279,7 +280,7 @@ public class ShopInformationActivity extends BaseActivity {
     }
 
 
-    @OnClick({R.id.iv_service, R.id.tv_join_shop_cat, R.id.tv_buy})
+    @OnClick({R.id.iv_service, R.id.tv_join_shop_cat, R.id.tv_buy,R.id.iv_fenxiang})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_service:
@@ -294,7 +295,39 @@ public class ShopInformationActivity extends BaseActivity {
             case R.id.tv_buy:
                 Toast.makeText(ShopInformationActivity.this, "直接购卖", Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.iv_fenxiang:
+//                Toast.makeText(ShopInformationActivity.this, "分享", Toast.LENGTH_SHORT).show();
+                showShare();
+                break;
         }
+    }
+
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不     调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
     }
 
 
